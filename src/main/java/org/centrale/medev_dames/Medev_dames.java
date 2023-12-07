a3f5ed1777ceee9a9166ad67e212754ce7bfeeb2
Sacha2023 <sachacruzabsi@gmail.com>
2023-12-07 09:23:20 +0100

    Initialization

---
+++ src/main/java/org/centrale/medev_dames/Medev_dames.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev_dames;
+
+/**
+ *
+ * @author sacha
+ */
+public class Medev_dames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
