db5c3b0d20e340a8f15b4674c979ec6db18b35f4
Sacha2023 <sachacruzabsi@gmail.com>
2023-12-07 10:24:27 +0100

    test commit

--- src/main/java/org/centrale/medev_dames/Damier.java
+++ src/main/java/org/centrale/medev_dames/Damier.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.centrale.medev_dames;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author sacha
  */
 public class Damier {
+    
+    private ArrayList<Pion> listePions;
+    private Joueur j1;
+    private Joueur j2;
+    private static final int LARGEUR = 10;
+    
+    public Damier(){
+        listePions = new ArrayList<>();
+        j1 = new Joueur();
+        j2 = new Joueur();
+    }
+    
+    public void init(){
+        
+        for (int i = 0; i < LARGEUR; i=i+2) {
+            listePions.add(new Pion(i,0));
+        }
+    }
+    
     
 }
--- src/main/java/org/centrale/medev_dames/Pion.java
+++ src/main/java/org/centrale/medev_dames/Pion.java
@@ -9,5 +9,19 @@ package org.centrale.medev_dames;
  * @author sacha
  */
 public class Pion {
+    int x;
+    int y;
+    //deplacer(listePions), manger(listePions),
+    public Pion(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
     
+    
+    
+    
+    public void test(){
+        System.out.println("test");
+    }
+
 }
