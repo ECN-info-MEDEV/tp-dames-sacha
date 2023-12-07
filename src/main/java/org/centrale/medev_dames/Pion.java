315dc5a6a85eff31fa63b7c49a5464c5026931b9
MarioEspinoza <mario.a.espinoza.mtz@gmail.com>
2023-12-07 03:15:08 -0600

    -Constructor/Attributes/methodes/ class Pion

--- src/main/java/org/centrale/medev_dames/Pion.java
+++ src/main/java/org/centrale/medev_dames/Pion.java
@@ -4,10 +4,80 @@
  */
 package org.centrale.medev_dames;
 
+import java.util.List;
+
 /**
  *
  * @author sacha
  */
 public class Pion {
+    //atributos
+    //---------------ATTRIBUTES---------------
+    private int x;
+    private int y;
+    private boolean dame;
+    //movimiento(List<pion>), comer(List<pion>)
+    //---------------CONSTRUCTORS--------------
+
+    public Pion() {
+        x=0;
+        y=0;
+        dame=false;
+    }
+
+    public Pion(int x, int y, boolean dame) {
+        this.x = x;
+        this.y = y;
+        this.dame = dame;
+    }
+
+    //---------------GETTERS & SETTERS---------
+    public int getX(){
+        return x;
+        
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isDame() {
+        return dame;
+    }
+    
+    public void setDame(boolean dame) {    
+        this.dame = dame;
+    }
+
+    //---------------METHODES------------------
+    /**
+     *
+     * @param pions
+     */
+    public void movement(List <Pion> pions) {
+        if (dame) {
+            
+        }
+    }
+    public void manger(List <Pion> pions){
+        
+    }
+    
+    public boolean peutDeplacer(){
+        if (this.getX()<0 || this.getX()<0) {
+            
+        }
+    }
+    public void positionOccupe(){
+        
+    }
     
 }
